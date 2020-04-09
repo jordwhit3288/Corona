8e91cb1f5860b3510005dd22526541067da93a97
jordwhit3288 <jordwhit3288@gmail.com>
2020-04-09 11:20:24 -0400

    have the daily cases for NC and NY on here now. cleaned some stuff

--- NorthCarolina.R
+++ NorthCarolina.R
@@ -107,6 +107,35 @@ plotly_build(nc_deaths_by_county)
 
 
 
+######################## LOOKING AT PEAK #########################
+times_corona_data  <- read_csv("ny_times_data/covid-19-data/us-states.csv")
+View(times_corona_data)
+
+head(times_corona_data)
+#Filtering df to NY data
+
+times_nc_data <- filter(times_corona_data, state == 'North Carolina')
+
+head(times_nc_data)
+
+times_nc_data <- times_nc_data %>% 
+  mutate(daily_increase = cases - lag(cases)) # dplyr has its own version of lag
+
+times_nc_data$daily_increase <- replace_na(times_nc_data$daily_increase, 0) 
+View(usa_cases)
+
+
+nc_daily_increase_plot <- ggplot(data = times_nc_data) +
+  geom_smooth(mapping = aes(x=date, y=daily_increase)) +
+  labs(title = "North Carolina Daily Case Increase", subtitle = "4/8/2020", x="Date", y = "Case Increase") +
+  scale_x_date(breaks = "5 days") + theme(axis.ticks = element_line(colour = "gray4"), 
+                                          panel.grid.major = element_line(linetype = "dotted"), 
+                                          axis.text.x = element_text(vjust = 0.5, 
+                                                                     angle = 45), panel.background = element_rect(fill = "aliceblue", 
+                                                                                                                  linetype = "longdash"), plot.background = element_rect(fill = "white"))
+nc_daily_increase_plot
+
+
 
 
 
---
+++ new_york.R
@@ -0,0 +1,26 @@
+times_corona_data  <- read_csv("ny_times_data/covid-19-data/us-states.csv")
+View(times_corona_data)
+
+head(times_corona_data)
+#Filtering df to NY data
+
+ny_data <- filter(times_corona_data, state == 'New York')
+
+head(ny_data)
+
+ny_data <- ny_data %>% 
+  mutate(daily_increase = cases - lag(cases)) # dplyr has its own version of lag
+
+ny_data$daily_increase <- replace_na(ny_data$daily_increase, 0) 
+View(usa_cases)
+
+
+ny_daily_increase_plot <- ggplot(data = ny_data) +
+  geom_smooth(mapping = aes(x=date, y=daily_increase)) +
+  labs(title = "NY Daily Case Increase", subtitle = "4/8/2020", x="Date", y = "Case Increase") +
+  scale_x_date(breaks = "5 days") + theme(axis.ticks = element_line(colour = "gray4"), 
+  panel.grid.major = element_line(linetype = "dotted"), 
+  axis.text.x = element_text(vjust = 0.5, 
+  angle = 45), panel.background = element_rect(fill = "aliceblue", 
+  linetype = "longdash"), plot.background = element_rect(fill = "white"))
+ny_daily_increase_plot
